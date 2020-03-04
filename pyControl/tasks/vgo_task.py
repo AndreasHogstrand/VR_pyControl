49dbefeb8ba3476e48388ccb4649e923eea7ba22
Andreas Hogstrand <aoh25@cam.ac.uk>
2020-03-04 14:30:19 +0000

    Cleaned up syntax, added dummy photodiode button

--- pyControl/tasks/vgo_task.py
+++ pyControl/tasks/vgo_task.py
@@ -11,6 +11,8 @@ board = Breakout_1_2()  # Instantiate the breakout board object.
 
 uart_basic = Uart_basic(board.port_1)
 
+pyboard_button = Digital_input('X17', falling_event='target_on/off', pull='up')  # USR button on pyboard, temporary
+
 
 # States and events.
 
@@ -28,95 +30,95 @@ v.inter_trial_duration = 1
 
 def white_noise(event):
 	"""Play white noise before trial start"""
-	if event == 'entry'
+	if event == 'entry':
 		#Play white noise
-		timed_goto_state('wait_for_target',white_noise_duration*second)
+		timed_goto_state('create_target', v.white_noise_duration*second)
 
 def create_target(event):
 	"""Send target command to unity and then wait for target to appear"""
-	if event == 'entry'
+	if event == 'entry':
 		v.target_position = randint(1,8) #Generate random target location
 		uart_basic.write(chr(v.target_position+96)) #Send character corresponding to target location (a-h) to Unity
-	elif event == 'target_on/off'
+	elif event == 'target_on/off':
 		set_timer('target_timeout', v.target_timeout_duration*second, output_event=True) #Begin timeout timer
 		goto_state('target'+str(v.target_position))
 
 def target1(event):
 	"""Wait for collision or timeout"""
-	if event == 'target on/off'
+	if event == 'target_on/off':
 		disarm_timer('target_timeout')
 		goto_state('success')
-	elif event = 'target_timeout'
+	elif event == 'target_timeout':
 		goto_state('failure')
 
 def target2(event):
 	"""Wait for collision or timeout"""
-	if event == 'target on/off'
+	if event == 'target_on/off':
 		disarm_timer('target_timeout')
 		goto_state('success')
-	elif event = 'target_timeout'
+	elif event == 'target_timeout':
 		goto_state('failure')
 
 def target3(event):
 	"""Wait for collision or timeout"""
-	if event == 'target on/off'
+	if event == 'target_on/off':
 		disarm_timer('target_timeout')
 		goto_state('success')
-	elif event = 'target_timeout'
+	elif event == 'target_timeout':
 		goto_state('failure')
 
 def target4(event):
 	"""Wait for collision or timeout"""
-	if event == 'target on/off'
+	if event == 'target_on/off':
 		disarm_timer('target_timeout')
 		goto_state('success')
-	elif event = 'target_timeout'
+	elif event == 'target_timeout':
 		goto_state('failure')
 
 def target5(event):
 	"""Wait for collision or timeout"""
-	if event == 'target on/off'
+	if event == 'target_on/off':
 		disarm_timer('target_timeout')
 		goto_state('success')
-	elif event = 'target_timeout'
+	elif event == 'target_timeout':
 		goto_state('failure')
 
 def target6(event):
 	"""Wait for collision or timeout"""
-	if event == 'target on/off'
+	if event == 'target_on/off':
 		disarm_timer('target_timeout')
 		goto_state('success')
-	elif event = 'target_timeout'
+	elif event == 'target_timeout':
 		goto_state('failure')
 
 def target7(event):
 	"""Wait for collision or timeout"""
-	if event == 'target on/off'
+	if event == 'target_on/off':
 		disarm_timer('target_timeout')
 		goto_state('success')
-	elif event = 'target_timeout'
+	elif event == 'target_timeout':
 		goto_state('failure')
 
 def target8(event):
 	"""Wait for collision or timeout"""
-	if event == 'target on/off'
+	if event == 'target_on/off':
 		disarm_timer('target_timeout')
 		goto_state('success')
-	elif event = 'target_timeout'
+	elif event == 'target_timeout':
 		goto_state('failure')
 
 def success(event):
 	"""Deliver reward after reward delay, then return to trial start after inter trial delay"""
-	if event == 'entry'
+	if event == 'entry':
 		set_timer('reward', v.reward_delay_duration*second, output_event=True) #Begin timeout timer
-	elif event == 'reward'
+	elif event == 'reward':
 		#play reward tone
 		#deliver reward
 		timed_goto_state('white_noise', v.inter_trial_duration*second)
 
 def failure(event):
 	"""Play failure tone, then return to trial start after inter trial delay"""
-	if event == 'entry'
+	if event == 'entry':
 		#play failure tone
 		timed_goto_state('white_noise', v.inter_trial_duration*second)
 
